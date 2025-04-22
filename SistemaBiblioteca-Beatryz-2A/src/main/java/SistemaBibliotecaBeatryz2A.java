4e6c02884bd9533c281029e7b89d9072786c879a
beatryz1284 <beatryzchagasdossantos1@gmail.com>
2025-04-22 12:29:26 -0300

    Add files via upload

---
+++ SistemaBiblioteca-Beatryz-2A/src/main/java/SistemaBibliotecaBeatryz2A.java
@@ -0,0 +1,12 @@
+
+public class SistemaBibliotecaBeatryz2A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        
+        
+    }
+    
+}
